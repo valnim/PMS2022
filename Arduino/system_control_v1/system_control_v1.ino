81d28420e7d1c67ab51a3e5ce296cb2d204b9590
Valentin Nimmervoll <valnim46@gmail.com>
2022-10-13 11:30:29 +0200

    Control With Elego Class

--- Arduino/system_control_v1/system_control_v1.ino
+++ Arduino/system_control_v1/system_control_v1.ino
@@ -1,7 +1,12 @@
+#include <Elego_Motor.h>
+
+
 // Motor control digital output pins defined as global constants
   const int mo1_controlPin1A = 2;                
   const int mo1_controlPin2A = 5;                 
   const int mo1_enablePin = 9;         
+  Elego_Motor mo1(mo1_controlPin1A, mo1_controlPin2A, mo1_enablePin);
+
 
 // Input bins 
   const int bstart_Pin = 4;         // Button Start Pin      
@@ -9,7 +14,7 @@
 
 // Motor control global variables: 
   //Motor 1
-  int mo1_mS = 200;                 // Motor speed 0..255
+  int mo1_mS = 250;                 // Motor speed 0..255
   bool mo1_mD = true;               // Forward (true) or reverse (false)
 
 // Button Variables
@@ -24,10 +29,11 @@ void setup()
   Serial.begin(9600); // Initialize Serial
 
   // Declare digital output pins:
-  pinMode(mo1_controlPin1A, OUTPUT);      // Motor 1/2 1A
-  pinMode(mo1_controlPin2A, OUTPUT);      // Motor 1/2 2A
-  pinMode(mo1_enablePin, OUTPUT);         // Motor 1/2 EN1,2
-  digitalWrite(mo1_enablePin, LOW);       // Motor 1/2 Motor off
+
+  // pinMode(mo1_controlPin1A, OUTPUT);      // Motor 1/2 1A
+  // pinMode(mo1_controlPin2A, OUTPUT);      // Motor 1/2 2A
+  // pinMode(mo1_enablePin, OUTPUT);         // Motor 1/2 EN1,2
+  // digitalWrite(mo1_enablePin, LOW);       // Motor 1/2 Motor off
   
   // Decleare digital input pins:
   pinMode(bstart_Pin, INPUT);              // Button Start
@@ -46,46 +52,48 @@ int runtime() {
   return secs;
 }
 
-void SetMotorControl(bool mD, int mS, int controlPin1A, int controlPin2A, int enablePin )
-{
- if (mD)            // Forward
-   {
-      digitalWrite(controlPin1A, HIGH);
-      digitalWrite(controlPin2A, LOW);
-      Serial.println(mS);
+// void SetMotorControl(bool mD, int mS, int controlPin1A, int controlPin2A, int enablePin )
+// {
+//  if (mD)            // Forward
+//    {
+//       digitalWrite(controlPin1A, HIGH);
+//       digitalWrite(controlPin2A, LOW);
+//       //Serial.println(mS);
       
-   }
- else               // Reverse
-   {
-      digitalWrite(controlPin1A, LOW);
-      digitalWrite(controlPin2A, HIGH);
-      Serial.println(mS);
-   } 
- analogWrite(enablePin, mS);  // Speed
-}
+//    }
+//  else               // Reverse
+//    {
+//       digitalWrite(controlPin1A, LOW);
+//       digitalWrite(controlPin2A, HIGH);
+//       //Serial.println(mS);
+//    } 
+//  analogWrite(enablePin, mS);  // Speed
+// }
 
-void SetMotorStop(bool mD, int reverseTime, int controlPin1A, int controlPin2A, int enablePin )
-{
-  if (mD)
-  {
-    digitalWrite(controlPin1A, LOW);
-    digitalWrite(controlPin2A, HIGH);
-    delay(reverseTime);
-    digitalWrite(controlPin2A, LOW);
-  }
-  else
-  {
-    digitalWrite(controlPin1A, HIGH);
-    digitalWrite(controlPin2A, LOW);
-    delay(reverseTime);
-    digitalWrite(controlPin1A, LOW);
-  }
-  analogWrite(enablePin, 0);
-}
+// void SetMotorStop(bool mD, int reverseTime, int controlPin1A, int controlPin2A, int enablePin )
+// {
+//   if (mD)
+//   {
+//     digitalWrite(controlPin1A, LOW);
+//     digitalWrite(controlPin2A, HIGH);
+//     delay(reverseTime);
+//     digitalWrite(controlPin2A, LOW);
+//   }
+//   else
+//   {
+//     digitalWrite(controlPin1A, HIGH);
+//     digitalWrite(controlPin2A, LOW);
+//     delay(reverseTime);
+//     digitalWrite(controlPin1A, LOW);
+//   }
+//   analogWrite(enablePin, 0);
+// }
 
 void mode_one()
 {
-  SetMotorControl(mo1_mD, mo1_mS, mo1_controlPin1A, mo1_controlPin2A, mo1_enablePin);
+  //SetMotorControl(mo1_mD, mo1_mS, mo1_controlPin1A, mo1_controlPin2A, mo1_enablePin);
+  mo1.setDirection(mo1_mD);
+  mo1.setSpeed(mo1_mS);
 }
 
 void loop() 
@@ -111,7 +119,8 @@ void loop()
     time = runtime();
   }
   else if (mode == 2){
-    SetMotorStop(!mo1_mD, 80, mo1_controlPin1A, mo1_controlPin2A, mo1_enablePin);
+    //SetMotorStop(!mo1_mD, 80, mo1_controlPin1A, mo1_controlPin2A, mo1_enablePin);
+    mo1.stop(80);
     digitalWrite(ledPin, LOW);
     mode = 0;
   }
