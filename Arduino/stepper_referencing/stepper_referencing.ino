51d359fbbb34cb1cb11e7b93df99a227d2ae8104
Valentin Nimmervoll <valnim46@gmail.com>
2022-10-27 13:28:25 +0200

    Stepper Positioning with Referencing

--- Arduino/stepper_referencing/stepper_referencing.ino
+++ Arduino/stepper_referencing/stepper_referencing.ino
@@ -1,47 +1,96 @@
 #include <AccelStepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution for your motor
+const int stepsPerRev = 200;  // change this to fit the number of steps per revolution for your motor
 
-// Interrrupt Pins
-const int interruptPin1 = 2;  // Stop Pin
-const int interruptPin2 = 3;  // Start Pin
+// Input Pins
+const int button1 = 2;  // Start Pin
+const int button2 = 3;  // Reference Button Pin
+
+// Output Pins
+const int ledPin =  13; // Indicatior LED Pin
 
 // Motor variables
-int mo1Speed = 400;
-bool mo1Running = false;
-int m01Direction = 1;
+int mo1Speed = 150;             // Motor 1 Base Speed 
+int mo1MaxSpeedMult = 2;        // Motor 1 Max Speed Multiplier
+int mo1Accel = 20000;           // Motor 1 Acceleration
+int m01Direction = 1;           // Motor 1 Standard Direction Variable (1 - Clockwise, -1 - Counterclockwise), Referencing happens in opposite direction
+int mo1Pos1 = stepsPerRev*2;    // Position 1 in relation to Reference Point
+int mo1Pos2 = stepsPerRev*3;    // Position 2 in relation to Reference Point
 
 // initialize the stepper library on pins 12 through 9:
-AccelStepper stepper1(4, 12, 11, 10, 9);
+AccelStepper stepper1(4, 12, 11, 10, 9);    // Definition of Stepper Motor (StepperType=4, PINA1 = 12, PINA2 = 11, PINA3 = 10, PINA4 = 9)
+
+// SFC Variables
+int mode = 0;     // Current SFC Mode State
 
 void setup() { 
   // initialize the serial port:
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(interruptPin2), start, RISING);
-  attachInterrupt(digitalPinToInterrupt(interruptPin1), stop, RISING);
+  // Initialize Inputs
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  if (mo1Running){
-    stepper1.setAcceleration(20000);
-    stepper1.setMaxSpeed(m01Direction*800);
-    stepper1.setSpeed(m01Direction*400);	
-    stepper1.runSpeed();
+  // Mode Switch Logic and Mode Logic that has to happen once
+  if (digitalRead(button1) == HIGH && mode == 0)                      //0-1 Requirement: Button Start
+  {
+    stepper1.setAcceleration(mo1Accel);
+    stepper1.setMaxSpeed(m01Direction*mo1Speed*2);
+    stepper1.setSpeed(-m01Direction*mo1Speed);	
+    mode = 1; // Switch Mode
+    Serial.println("Start Referencing");
   }
-  
-}
-
-void stop(){
-  if (mo1Running){
+  if (digitalRead(button2) && mode == 1)                              //1-2 Reference Sensor Reached
+  {
+    // Stopp Motor at Reference Position
     stepper1.stop();
-    mo1Running = false;  
+    Serial.println(stepper1.currentPosition());     // Log Position
+    stepper1.setCurrentPosition(0);                 // Set Current Position to Zero
+    Serial.println(stepper1.currentPosition());     // Log Position
+    delay(1000);   // Wait at Reference Position
+    // Setup motor settings for movement towards Pos1
+    stepper1.setAcceleration(mo1Accel);
+    stepper1.setMaxSpeed(m01Direction*mo1Speed*2);
+    stepper1.setSpeed(m01Direction*mo1Speed);	
+    mode = 2; // Switch Mode
+    Serial.println("Reference Position Reached");
   }
-  else{
-    m01Direction = -1 * m01Direction;
+  else if (abs(stepper1.currentPosition()) >= mo1Pos1 && mode == 2)        // 2-3 Requirement: Position 1 Reached
+  {
+    // Stop Motor at Pos 1
+    stepper1.stop();
+    Serial.println(abs(stepper1.currentPosition()));  // Log Current Position
+    delay(1000);  // Wait at Position 1
+    // Setup motor settings for movement towards Pos2
+    stepper1.setAcceleration(mo1Accel);
+    stepper1.setMaxSpeed(m01Direction*mo1Speed*2);
+    stepper1.setSpeed(m01Direction*mo1Speed);	
+    mode = 3; // Switch Mode
+    Serial.println("Pos 1 Reached");
   }
+  else if (abs(stepper1.currentPosition()) >= mo1Pos2 && mode == 3)         // 3-4 Requirement: Position 2 Reached
+  {
+    // Stop Motor at Pos 2
+    stepper1.stop();
+    Serial.println(abs(stepper1.currentPosition()));  // Log Current Position
+    delay(500); // Wait at Position 2
+    mode = 0; // Switch Mode
+    Serial.println("Pos 2 reached");
+  }
+ 
+  // Mode Logic that has to be run each cycle
+  if (mode == 1){                                             // 1 Referencing
+    stepper1.runSpeed();  // refreshes Motor control
+  }
+  else if (mode == 2){                                        // 2 Move To Position 1 
+    stepper1.runSpeed();  // refreshes Motor control
+  }
+  else if (mode == 3){                                        // 2 Move To Position 2
+    stepper1.runSpeed();  // refreshes Motor control
+  }
+  
   
 }
 
-void start(){
-  mo1Running = true;
-}
