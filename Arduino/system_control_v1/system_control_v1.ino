4e122c39cd8f4d81593a622ba925a610980168b6
Valentin Nimmervoll <valnim46@gmail.com>
2022-10-27 14:15:22 +0200

    Improve Documentation of SystemControlV1

--- Arduino/system_control_v1/system_control_v1.ino
+++ Arduino/system_control_v1/system_control_v1.ino
@@ -1,48 +1,46 @@
 #include <Elego_Motor.h>
 
-
 // Motor control digital output pins defined as global constants
+  // Pins on Channel 1 of L293D
   const int mo1_controlPin1A = 2;                
   const int mo1_controlPin2A = 5;                 
   const int mo1_enablePin = 9;      
   Elego_Motor mo1(mo1_controlPin1A, mo1_controlPin2A, mo1_enablePin);
 
-  const int mo2_controlPin1A = 8;                
-  const int mo2_controlPin2A = 10;                 
-  const int mo2_enablePin = 11;      
-  Elego_Motor mo2(mo2_controlPin1A, mo2_controlPin2A, mo2_enablePin);
+  // Pins on Channel 2 of L293D Currently not used!
+  //const int mo2_controlPin1A = 8;                
+  //const int mo2_controlPin2A = 10;                 
+  //const int mo2_enablePin = 11;      
+  //Elego_Motor mo2(mo2_controlPin1A, mo2_controlPin2A, mo2_enablePin);
 
 // Input pins 
   const int bstart_Pin = 4;         // Button Start Pin
   const int pResistor = A0;         // Photoresistor at Arduino analog pin A0
 
-// Output pins
-  const int ledPin =  13;           // Indication LED
-
-
 // Motor control global variables: 
-  //Motor 1
+  //Motor 1 (Transportsystem 1 both motors)
   int mo1_mS = 120;                 // Motor speed 0..255
-  bool mo1_mD = false;               // Forward (true) or reverse (false)
-  int mo2_mS = 120;                 // Motor speed 0..255
-  bool mo2_mD = false;               // Forward (true) or reverse (false)
+  bool mo1_mD = false;              // Forward (true) or reverse (false)
+  //Motor 2
+  //int mo2_mS = 120;                 // Motor speed 0..255
+  //bool mo2_mD = false;              // Forward (true) or reverse (false)
 
 // Button Variables
   int bstart_State = 0;             // Button Start Default State
-  int count_State = 0;             // Button Count Default State
-  int count_lastState = 0;         // Button Count Last State
+  int count_State = 0;              // Button Count Default State
+  int count_lastState = 0;          // Button Count Last State
 
 // General Variables
   int mode = 0;                     // System Control Mode
   int count = 0;                    // Counter
-  int count_max = 50;                // Max goods count in Transport Box
+  int count_max = 20;               // Max goods count in Transport Box
 
   bool calibrated = false;          // Light barrier calibration status
   int threshold = 0;                // Light barrier threshold
   const int threshold_offset = 100; // Threshold offset, the lower the offset the higher the sensitivity
   const int num_calibrate = 10;     // Number of values that are middled
   int idx = 0;                      // Index variable
-  int value = 0;
+  int barrierValue = 0;             // Light barrier sensor value
 
 void setup() 
 {
@@ -50,33 +48,32 @@ void setup()
   
   // Decleare digital input pins:
   pinMode(bstart_Pin, INPUT);       // Button Start
-  pinMode(ledPin, OUTPUT);          // Indication LED
-  pinMode(pResistor, INPUT);        // Set pResistor 
+  pinMode(pResistor, INPUT);        // pResistor 
 }
 
-void calibrate()
+void calibrate()    // Before each counting cycle the light barrier is calibrated
 {
-  Serial.println(value);
-  threshold = threshold + value;
+  Serial.println(barrierValue);
+  threshold = threshold + barrierValue;
   idx = idx + 1;
-  if (idx >= num_calibrate){
+  if (idx >= num_calibrate){                      // The arithmetic median of num_calibrate is calculated
     threshold = threshold / num_calibrate;
     Serial.println("Sensor calbirated");
     Serial.println(threshold);
-    threshold = threshold - threshold_offset;
+    threshold = threshold - threshold_offset;     // The Sensor detection threshold is the median minus a pre determined offset
     calibrated = true; 
   }
   delay(100);
 }
 
-void counter(){
-  if (value < threshold){
+void counter(){   // If the barrier sensor value falls below the threshold an item is counted.
+  if (barrierValue < threshold){        
     count_State = HIGH;
   }
   else{
     count_State = LOW;
   }
-  // compare the bcount_State to its previous state
+  // compare the count_State to its previous state
   if (count_State != count_lastState) {
     // if the state has changed, increment the counter
     if (count_State == HIGH) 
@@ -93,15 +90,12 @@ void ts1_on()   // Starts Transport System 1
 {
   mo1.setDirection(mo1_mD);
   mo1.setSpeed(mo1_mS);
-  mo2.setDirection(mo2_mD);
-  mo2.setSpeed(mo2_mS);
+  //mo2.setDirection(mo2_mD);
+  //mo2.setSpeed(mo2_mS);
 }
 
 void loop() 
 {  
-  //Serial.println("Mode:");
-  //Serial.println(mode);
-  
   // read the state of the pushbutton value:
   bstart_State = digitalRead(bstart_Pin);
 
@@ -114,29 +108,27 @@ void loop()
   if (calibrated && mode == 1)                                //1-2 Sensor Calibrated
   {
     mode = 2;
-    ts1_on();
-    digitalWrite(ledPin, HIGH);
+    ts1_on();   // Turn the motors of Transporation System 1 on
   }
   else if (count >= count_max && mode == 2)                   // 2-3 Requirement: Counter reaches count_max
   {
     mode = 3;
-    mo1.stop(80);
-    mo2.stop(80);
-    digitalWrite(ledPin, LOW);
+    mo1.stop(80); // Stop motor1  of TS1
+    //mo2.stop(80);
     mode = 0;
     count = 0;
   }
  
   // Mode Logic that has to be run each cycle
   if (mode == 1){
-    value = analogRead(pResistor);
-    calibrate();
+    barrierValue = analogRead(pResistor); // current light barrier sensor value
+  calibrate();                            // call of calibration algorithm
   }
   else if (mode == 2){    // Motor 1 for Transport System 1 and Divider is ON, Counter is counting goods
-    value = analogRead(pResistor);
-    counter();
+    barrierValue = analogRead(pResistor);   // current light barrier sensor value
+    counter();                              // counter logic checks if light barrier detects goods
     Serial.println(count);
-    Serial.println(value);
+    Serial.println(barrierValue);
   }
   else if (mode == 3){    // After counter reaches desired value the transportsystem 1 stops, counter is resette
   }
