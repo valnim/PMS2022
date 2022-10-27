c86d09f2852bd4a9994a2592618ba5f8d06f082e
Valentin Nimmervoll <valnim46@gmail.com>
2022-10-27 11:52:10 +0200

    Non blocking stepper prototype

--- Arduino/stepper_prototype/stepper_prototype/stepper_prototype.ino
+++ Arduino/stepper_prototype/stepper_prototype/stepper_prototype.ino
@@ -1,103 +1,47 @@
-
-/*
- Stepper Motor Control - one revolution
-
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
-
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
-
-
- Created 11 Mar. 2007
- Modified 30 Nov. 2009
- by Tom Igoe
-
- */
-
-//#include <Stepper.h>
 #include <AccelStepper.h>
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution for your motor
 
 // Interrrupt Pins
-const int interruptPin1 = 2;  // Start Stop Pin
-const int interruptPin2 = 3;  // Reverse Direction Pin
+const int interruptPin1 = 2;  // Stop Pin
+const int interruptPin2 = 3;  // Start Pin
 
 // Motor variables
-int speed = 120;
-int revs = 25;
-bool direction = true;
-bool running = false;
+int mo1Speed = 400;
+bool mo1Running = false;
+int m01Direction = 1;
 
+// initialize the stepper library on pins 12 through 9:
+AccelStepper stepper1(4, 12, 11, 10, 9);
 
-// initialize the stepper library on pins 8 through 11:
-//Stepper myStepper(stepsPerRevolution, 12, 11, 10, 9);
-AccelStepper myStepper(4, 12, 11, 10, 9);
-
-void setup() {
-  // set the speed at 60 rpm:
-  //myStepper.setSpeed(speed); // max 200
-  myStepper.setMaxSpeed(800);
-  myStepper.setSpeed(20);	
-  myStepper.setAcceleration(20000);
+void setup() { 
   // initialize the serial port:
   Serial.begin(9600);
-  
-  //attachInterrupt(digitalPinToInterrupt(interruptPin2), changeDir, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin2), start, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin1), stop, RISING);
 }
 
 void loop() {
-  //Serial.println(running);
-  //Serial.println(direction);
-  
-  if (digitalRead(interruptPin2)){
-    attachInterrupt(digitalPinToInterrupt(interruptPin1), stop, RISING);
-    if (direction){
-      myStepper.runToNewPosition(myStepper.currentPosition()+revs*stepsPerRevolution);
-      //myStepper.step(revs*stepsPerRevolution);
-    }
-    else{
-      myStepper.runToNewPosition(myStepper.currentPosition()-revs*stepsPerRevolution);
-      //myStepper.step(-revs*stepsPerRevolution);
-    }
-    detachInterrupt(digitalPinToInterrupt(interruptPin1));
+  if (mo1Running){
+    stepper1.setAcceleration(20000);
+    stepper1.setMaxSpeed(m01Direction*800);
+    stepper1.setSpeed(m01Direction*400);	
+    stepper1.runSpeed();
   }
-
-
-  // // step one revolution  in one direction:
-  // Serial.println("clockwise");
-  // myStepper.step(20*stepsPerRevolution);
-  // delay(5000);
-
-  // // step one revolution in the other direction:
-  // Serial.println("counterclockwise");
-  // myStepper.step(-20*stepsPerRevolution);
-  // delay(5000);
+  
 }
 
 void stop(){
-  Serial.println("STOP");
-  //myStepper.step(0);
-  myStepper.stop();
-  // Serial.println("Button 1");
-  // if (running){
-  //   running = false;
-  //   myStepper.step(0);
-  // }
-  // else{
-  //   running = true;
-  //   if (direction){
-  //     myStepper.step(revs*stepsPerRevolution);
-  //   }
-  //   else{
-  //     myStepper.step(-revs*stepsPerRevolution);
-  //   }
-  //   running = false;
-  // }
+  if (mo1Running){
+    stepper1.stop();
+    mo1Running = false;  
+  }
+  else{
+    m01Direction = -1 * m01Direction;
+  }
+  
 }
 
-void changeDir(){
-  Serial.println("Button 2");
-  direction = !direction;
+void start(){
+  mo1Running = true;
 }
