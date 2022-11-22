7692358ef3e3be54359f6a7075b328dfcdeb3a36
Valentin Nimmervoll <valnim46@gmail.com>
2022-11-22 10:46:52 +0100

    Create Elego_Stepper Library

---
+++ Software/libraries/Elego_Stepper/Elego_Stepper.cpp
@@ -0,0 +1,42 @@
+#include "Arduino.h"
+#include "Elego_Stepper.h"
+Elego_Stepper::Elego_Stepper(int stepPin, int resetPin) {
+	pinMode(stepPin, OUTPUT);
+	pinMode(resetPin, OUTPUT);
+	
+	_stepPin = stepPin;
+	_resetPin = resetPin;
+	_running = false;
+	reset();
+}
+void Elego_Stepper::start(){
+	if (!_running)                   // Start (Move to Step 1)
+	  {
+		digitalWrite(_stepPin, HIGH);
+		delay(10);
+		digitalWrite(_stepPin, LOW);
+		_running = true;
+	  }
+}
+void Elego_Stepper::stop(int reverseTime){
+	if (_running)                   // Stop (Move to Step 0)
+	  {
+		digitalWrite(_stepPin, HIGH);
+		delay(10);
+		digitalWrite(_stepPin, LOW);
+		delay(10);
+		digitalWrite(_stepPin, HIGH);
+		delay(10);
+		digitalWrite(_stepPin, LOW);
+		delay(reverseTime);
+		digitalWrite(_stepPin, HIGH);
+		delay(10);
+		digitalWrite(_stepPin, LOW);
+		_running = false;
+	  }
+}
+void Elego_Stepper::reset(){
+	digitalWrite(_resetPin, HIGH);	// Reset Motor Driver
+	delay(10);
+	digitalWrite(_resetPin, LOW);
+}
