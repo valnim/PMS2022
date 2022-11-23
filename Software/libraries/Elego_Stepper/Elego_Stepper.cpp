f6a8426a76d53aee83c817b4e1009bf1088770ef
Valentin Nimmervoll <valnim46@gmail.com>
2022-11-23 15:48:15 +0100

    Update Elego Stepper Library

--- Software/libraries/Elego_Stepper/Elego_Stepper.cpp
+++ Software/libraries/Elego_Stepper/Elego_Stepper.cpp
@@ -2,18 +2,19 @@
 #include "Elego_Stepper.h"
 Elego_Stepper::Elego_Stepper(int stepPin, int resetPin) {
 	pinMode(stepPin, OUTPUT);
-	pinMode(resetPin, OUTPUT);
-	
+	pinMode(resetPin, OUTPUT);	
 	_stepPin = stepPin;
 	_resetPin = resetPin;
 	_running = false;
+	digitalWrite(_stepPin, LOW);
+	digitalWrite(_resetPin, HIGH);
 	reset();
 }
 void Elego_Stepper::start(){
 	if (!_running)                   // Start (Move to Step 1)
 	  {
 		digitalWrite(_stepPin, HIGH);
-		delay(10);
+		delay(100);
 		digitalWrite(_stepPin, LOW);
 		_running = true;
 	  }
@@ -22,21 +23,22 @@ void Elego_Stepper::stop(int reverseTime){
 	if (_running)                   // Stop (Move to Step 0)
 	  {
 		digitalWrite(_stepPin, HIGH);
-		delay(10);
+		delay(20);
 		digitalWrite(_stepPin, LOW);
-		delay(10);
+		delay(20);
 		digitalWrite(_stepPin, HIGH);
-		delay(10);
+		delay(20);
 		digitalWrite(_stepPin, LOW);
 		delay(reverseTime);
 		digitalWrite(_stepPin, HIGH);
-		delay(10);
+		delay(20);
 		digitalWrite(_stepPin, LOW);
 		_running = false;
 	  }
 }
 void Elego_Stepper::reset(){
 	digitalWrite(_resetPin, LOW);	// Reset Motor Driver
-	delay(10);
+	delay(500);
 	digitalWrite(_resetPin, HIGH);
+	_running = false;
 }
