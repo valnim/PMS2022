d1c495b1e55e850db8286ac1ed2b415e9d977d09
aloisjauernig <115631101+aloisjauernig@users.noreply.github.com>
2022-12-13 10:35:49 +0100

    Adaption of Position 2 of Lift-Axis

--- Software/control_software/control_software.ino
+++ Software/control_software/control_software.ino
@@ -79,7 +79,7 @@ int moADirection = -1;                  // Motor A Standard Direction Variable (
 const int phiPos1 = stepsPerRevY*2;     // Position 1 for Motor 2 in Phi-Axis
 const int phiPos2 = stepsPerRevY*8.2;   // Position 2 for Motor 2 in Phi-Axis
 const int xPos1 = stepsPerRevX*40;      // Position 1 for Motor 1 in Lift-Axis
-const int xPos2 = stepsPerRevX*2.5;     // Position 2 for Motor 1 in Lift-Axis
+const int xPos2 = stepsPerRevX*1;       // Position 2 for Motor 1 in Lift-Axis
 
 //initialize the stepper motors as existing objects
 AccelStepper stepperX(1, stepPinX, dirPinX);    // Crane Lift Motor
